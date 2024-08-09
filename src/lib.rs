6ad79de9370809d756bb5e737f3880c6ef8d9e8a
Jeonghyeon Kim <powergee101@gmail.com>
2024-08-09 14:05:18 +0900

    Remove jemalloc

--- src/lib.rs
+++ src/lib.rs
@@ -2,57 +2,25 @@
 #![feature(strict_provenance_atomic_ptr, strict_provenance)]
 #![feature(cfg_sanitize)]
 
+#[allow(unused_imports)]
 #[macro_use]
 extern crate cfg_if;
 
-cfg_if! {
-    if #[cfg(all(not(feature = "sanitize"), target_os = "linux"))] {
-        extern crate tikv_jemallocator;
-        #[global_allocator]
-        static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+pub struct MemSampler {}
 
-        extern crate tikv_jemalloc_ctl;
-        pub struct MemSampler {
-            epoch_mib: tikv_jemalloc_ctl::epoch_mib,
-            allocated_mib: tikv_jemalloc_ctl::stats::allocated_mib,
-        }
+impl Default for MemSampler {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
-        impl Default for MemSampler {
-            fn default() -> Self {
-                Self::new()
-            }
-        }
-
-        impl MemSampler {
-            pub fn new() -> Self {
-                MemSampler {
-                    epoch_mib: tikv_jemalloc_ctl::epoch::mib().unwrap(),
-                    allocated_mib: tikv_jemalloc_ctl::stats::allocated::mib().unwrap(),
-                }
-            }
-            pub fn sample(&self) -> usize {
-                self.epoch_mib.advance().unwrap();
-                self.allocated_mib.read().unwrap()
-            }
-        }
-    } else {
-        pub struct MemSampler {}
-
-        impl Default for MemSampler {
-            fn default() -> Self {
-                Self::new()
-            }
-        }
-
-        impl MemSampler {
-            pub fn new() -> Self {
-                println!("NOTE: Memory usage benchmark is supported only for linux.");
-                MemSampler {}
-            }
-            pub fn sample(&self) -> usize {
-                0
-            }
-        }
+impl MemSampler {
+    pub fn new() -> Self {
+        println!("NOTE: Memory usage benchmark is supported only for linux.");
+        MemSampler {}
+    }
+    pub fn sample(&self) -> usize {
+        0
     }
 }
 
